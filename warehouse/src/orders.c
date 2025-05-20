46d65052593779199a2efda76059416ed53c7cf6
Jorge Arbach <jorge.arbach@mi.unc.edu.ar>
2025-05-19 21:08:47 -0300

    fix hub/authentication.c and warehouse/orders.c

--- warehouse/src/orders.c
+++ warehouse/src/orders.c
@@ -1,22 +1,21 @@
 #include "orders.h"
 #include "inventory.h"
 
+#include "inventory.h"
+#include "orders.h"
+
 int send_orderdispatch_to_server(int sock, const char* order_id, const char* items_needed)
 {
-    // Create the JSON object for the response
     cJSON* response = cJSON_CreateObject();
 
-    // Add fields to the JSON object
     cJSON_AddStringToObject(response, "type", "order_dispatch");
     cJSON_AddStringToObject(response, "order_id", order_id);
     cJSON_AddStringToObject(response, "status", "Shipped");
 
-    // Generate a timestamp
     char timestamp[MAX_TIMESTAMP_LENGTH];
     generate_timestamp_wh(timestamp, sizeof(timestamp));
     cJSON_AddStringToObject(response, "timestamp", timestamp);
 
-    // Parse the items_needed string into a cJSON array
     cJSON* itemsArray = cJSON_Parse(items_needed);
     if (itemsArray == NULL || !cJSON_IsArray(itemsArray))
     {
@@ -25,16 +24,15 @@ int send_orderdispatch_to_server(int sock, const char* order_id, const char* ite
         return -1;
     }
 
-    // Add the items_needed array to the JSON object
     cJSON_AddItemToObject(response, "items_shipped", itemsArray);
 
-    // Serialize the JSON object to a string
     char* json_string = cJSON_PrintUnformatted(response);
 
-    // Send the JSON string to the server
     if (send_message_from_wh(sock, json_string) < 0)
     {
         perror("[ERROR] Failed to send inventory to server");
+        free(json_string);
+        cJSON_Delete(response);
         return -1;
     }
     else
@@ -42,9 +40,8 @@ int send_orderdispatch_to_server(int sock, const char* order_id, const char* ite
         printf("[INFO] Inventory sent to server: %s\n", json_string);
     }
 
-    // Clean up
     free(json_string);
     cJSON_Delete(response);
 
-    return 0; // Success
+    return 0;
 }
